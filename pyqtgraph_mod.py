a4d5d2563b919b38bd02ad71b581b12c419a215a
Miaoqi Chu mac <mqichu@anl.gov>
2021-04-25 15:49:26 -0500

    replace with pyqtgraph

---
+++ pyqtgraph_mod.py
@@ -0,0 +1,45 @@
+import pyqtgraph as pg
+from mpl_cmaps_in_ImageItem import pg_get_cmap
+import matplotlib.pyplot as plt
+
+
+class ImageViewROI(pg.ImageView):
+    def __init__(self, *arg, **kwargs):
+        super(ImageViewROI, self).__init__(*arg, **kwargs)
+        self.removeItem(self.roi)
+        self.roi = []
+        self.ui.roiBtn.setDisabled(True)
+    
+    def roiClicked(self):
+        pass
+
+    def roiChanged(self):
+        pass
+
+    def adjust_viewbox(self):
+        vb = self.getView()
+        xMin, xMax = vb.viewRange()[0]
+        yMin, yMax = vb.viewRange()[1]
+
+        vb.setLimits(xMin=xMin,
+                     xMax=xMax,
+                     yMin=yMin,
+                     yMax=yMax,
+                     minXRange=(xMax - xMin) / 50,
+                     minYRange=(yMax - yMin) / 50)
+        vb.setMouseMode(vb.RectMode)
+        vb.setAspectLocked(1.0)
+
+    def reset_limits(self):
+        """
+        reset the viewbox's limits so updating image won't break the layout;
+        """
+        self.view.state['limits'] = {'xLimits': [None, None],
+                                     'yLimits': [None, None],
+                                     'xRange': [None, None],
+                                     'yRange': [None, None]
+                                     } 
+
+    def set_colormap(self, cmap):
+        pg_cmap = pg_get_cmap(plt.get_cmap(cmap))
+        self.setColorMap(pg_cmap)
